aab96775b00b59f4ad1aab05ea08bc2455141bc8
Alexandra2628 <64959674+Alexandra2628@users.noreply.github.com>
2020-05-21 13:26:32 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -4,3 +4,9 @@ function aratamesaje() {
 function aratamesaj2() {
                                  alert("ce cauți aici?");
 }
+function cineesti(){
+prompt(" Care este numele tau?");
+}
+function catianiai(){
+prompt(" Câți ani ai?");
+} 
